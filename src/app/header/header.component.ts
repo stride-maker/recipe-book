5d3d134ca996a34e51207935ddb12f6365b707cb
Mohit Chaudahry <mohitch.work@gmail.com>
2021-08-23 19:56:41 +0530

    integrated with firebase
    integrated with firebase

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,15 @@ import { Component, OnInit, Output } from '@angular/core';
 export class HeaderComponent implements OnInit {
   collapsed: boolean = true;
 
-  constructor() {}
+  constructor(private dataStorageServive: DataStorageService) {}
 
   ngOnInit(): void {}
+
+  onSaveData() {
+    this.dataStorageServive.storeRecipes();
+  }
+
+  onFetchData() {
+    this.dataStorageServive.loadRecipes().subscribe();
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,22 +10,28 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Schnitzel',
-      'Super tasty schnitzel -- just awesome!',
-      'https://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-5.jpg',
-      [new Ingredient('Meat', 1), new Ingredient('French Fries', 5)]
-    ),
-    new Recipe(
-      'Big Fat Burger',
-      'What else you need to say ?',
-      'https://hips.hearstapps.com/del.h-cdn.co/assets/16/24/1465922855-delish-burgers-guacamole-1.jpg',
-      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Tasty Schnitzel',
+  //     'Super tasty schnitzel -- just awesome!',
+  //     'https://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-5.jpg',
+  //     [new Ingredient('Meat', 1), new Ingredient('French Fries', 5)]
+  //   ),
+  //   new Recipe(
+  //     'Big Fat Burger',
+  //     'What else you need to say ?',
+  //     'https://hips.hearstapps.com/del.h-cdn.co/assets/16/24/1465922855-delish-burgers-guacamole-1.jpg',
+  //     [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+  //   ),
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
