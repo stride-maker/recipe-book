898723eb1b6df55d094f03abc24c2b45a1fb8582
Mohit Chaudahry <mohitch.work@gmail.com>
2021-08-24 20:22:21 +0530

    authentication implemented
    authentication implemented

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+import { AuthService } from './auth.service';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean | Promise<boolean> | Observable<boolean | UrlTree> {
+    return this.authService.user.pipe(
+      take(1),
+      map((user) => {
+        const isAuth = user ? true : false;
+        if (isAuth) {
+          return true;
+        } else {
+          return this.router.createUrlTree(['/auth']);
+        }
+      })
+    );
+  }
+}
